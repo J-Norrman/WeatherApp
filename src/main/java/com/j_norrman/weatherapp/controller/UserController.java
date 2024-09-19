033ca6387b3ae4068418f2958b29f2d55dc68e23
Joel Norrman <joel-norrman@hotmail.com>
2024-09-19 10:43:37 +0200

    Add: GetMapping for all users

--- src/main/java/com/j_norrman/weatherapp/controller/UserController.java
+++ src/main/java/com/j_norrman/weatherapp/controller/UserController.java
@@ -1,9 +1,14 @@
 package com.j_norrman.weatherapp.controller;
 
+import com.j_norrman.weatherapp.model.user.User;
 import com.j_norrman.weatherapp.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/user")
@@ -12,6 +17,10 @@ public class UserController {
     @Autowired
     public UserController(UserRepository userRepository) {
         this.userRepository = userRepository;
+    }
+    @GetMapping
+    public ResponseEntity<List<User>> getUsers() {
+        return ResponseEntity.ok(userRepository.findAll());
     }
 
 
