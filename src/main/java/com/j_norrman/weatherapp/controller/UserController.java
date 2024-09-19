5568c3ecf740c1900745e2414d5db2293c0825eb
Joel Norrman <joel-norrman@hotmail.com>
2024-09-19 14:04:32 +0200

    Add: UserService, add user functionality

--- src/main/java/com/j_norrman/weatherapp/controller/UserController.java
+++ src/main/java/com/j_norrman/weatherapp/controller/UserController.java
@@ -2,27 +2,34 @@ package com.j_norrman.weatherapp.controller;
 
 import com.j_norrman.weatherapp.model.user.User;
 import com.j_norrman.weatherapp.repository.UserRepository;
+import com.j_norrman.weatherapp.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
 @RequestMapping("/user")
 public class UserController {
-    private final UserRepository userRepository;
-    @Autowired
-    public UserController(UserRepository userRepository) {
-        this.userRepository = userRepository;
-    }
-    @GetMapping
-    public ResponseEntity<List<User>> getUsers() {
-        return ResponseEntity.ok(userRepository.findAll());
+
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
     }
 
+    @GetMapping
+    public ResponseEntity<List<User>> getUsers() {
+        return ResponseEntity.ok(userService.getAllUsers());
+    }
+
+    @PostMapping
+    public ResponseEntity<User> createUser(@RequestBody User user) {
+        User savedUser = userService.createUser(user);
+        return ResponseEntity.status(HttpStatus.CREATED).body(savedUser);
+    }
 
 
 }
---
+++ src/main/java/com/j_norrman/weatherapp/service/UserService.java
@@ -0,0 +1,25 @@
+package com.j_norrman.weatherapp.service;
+
+import com.j_norrman.weatherapp.model.user.User;
+import com.j_norrman.weatherapp.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UserService {
+
+    private final UserRepository userRepository;
+
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+    public List<User> getAllUsers() {
+        return userRepository.findAll();
+    }
+
+    public User createUser(User user) {
+        return userRepository.save(user);
+    }
+}
